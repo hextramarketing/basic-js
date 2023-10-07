f49c623b81c922e2c3e812bcb8a39110bb622aa5
Hextra <support@hextra.co>
2023-10-07 20:21:08 +0700

    demo

--- video2/video2.js
+++ video2/video2.js
@@ -1,1 +1,5 @@
-console.log("hello");
+console.log(`hel""''lo`);
+let x = 5,
+  y = 6,
+  z = 7;
+console.log("x =", x, "y =", y, "z =", z);
---
+++ video4/video4.js
@@ -0,0 +1,3 @@
+let a = "tôi là một chuỗi";
+console.log(a);
+console.log(4 + 5 + a);
